4f79e2bc792e1a1786f042d1490c069ee93a1f23
Prabin Shrestha <41283995+Prabinshrestha737@users.noreply.github.com>
2022-07-02 13:02:37 -0400

    Face detection and recognization

---
+++ histogram.py
@@ -0,0 +1,47 @@
+import colorsys
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv.imread('red.jpg')
+cv.imshow('Red SandalWood', img)
+
+blank = np.zeros(img.shape[:2], dtype='uint8')
+
+#Gray 
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('GRAY', gray)
+
+# circle = cv.circle(blank, (img.shape[1]//2, img.shape[0]//2), 100, 255, -1)
+
+
+# mask = cv.bitwise_and(gray, gray, mask=circle)
+# cv.imshow('Mask', mask)
+
+
+
+#GRayScale histogram
+# gray_hist = cv.calcHist([gray], [0], None, [256], [0,256])
+
+# plt.figure()
+# plt.title('GrayScale Histogram')
+# plt.xlabel('Bins')
+# plt.ylabel('# of pixels')
+# plt.plot(gray_hist)
+# plt.xlim([0, 256])
+# plt.show()
+# plt.plot()
+
+
+
+#Color Histogram 
+
+colors = ('b', 'g', 'r')
+for i, col in enumerate(colors):
+    hist = cv.calcHist([img], [i], None, [256], [0,256])
+    plt.plot(hist, color=col)
+    plt.xlim([0, 256])
+
+plt.show()
+
+cv.waitKey(0)
